fa4a95b139503100d36ddaf95e6e9de3ff6bc7ab
ZheniaKv <zhenia@mail.com>
2020-05-12 00:43:36 +0300

    the cart page was made and it shows the product selected by the user

--- carts/views.py
+++ carts/views.py
@@ -19,11 +19,10 @@ class ProductInSession(View):
             request.session['css']['style'] = css_rule
             request.session.modified = True
 
-
         if 'cart' not in request.session:
             request.session['cart'] = {}
-            request.session['cart']['product_id'] = object.id
-            request.session['cart']['quantity'] = quantity
+            request.session['cart']['product_id']= object.id
+            request.session['cart']['quantity']= quantity
             request.session.modified = True
             return HttpResponse(" ")
 
@@ -34,10 +33,23 @@ class ProductInSession(View):
             return HttpResponse(" ")
 
 
-
 class ToCart(TemplateView):
 
     template_name = 'carts/cart.html'
+
+    def get_context_data(self, *args, **kwargs):
+
+                context = super(ToCart,
+                                self).get_context_data(**kwargs)
+                product_id = self.request.session['cart']['product_id']
+                quantity = self.request.session['cart']['quantity']
+                current_product = Product.objects.get(id=product_id)
+                price = current_product.price
+                context['product']=current_product
+                context['quantity'] = quantity
+                context['price'] = price
+
+                return context
 
 
 
